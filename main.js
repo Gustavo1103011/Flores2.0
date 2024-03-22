fe113cb484aff90373f63d4d2ffa8cd0003a4867
Gustavo110301 <gustavo.hernandeza@alumno.buap>
2024-03-21 22:43:43 -0600

    Subida

---
+++ main.js
@@ -0,0 +1,21 @@
+onload = () =>{
+        document.body.classList.remove("container");
+};
+
+// const { exec } = require('child_process');
+
+// // Comando para ejecutar el script de Python
+// const comandoPython = 'Flor.py'; // Reemplaza 'script.py' con el nombre de tu script Python
+
+// // Ejecutar el comando
+// exec(comandoPython, (error, stdout, stderr) => {
+//   if (error) {
+//     console.error(`Error al ejecutar el script de Python: ${error.message}`);
+//     return;
+//   }
+//   if (stderr) {
+//     console.error(`Error en el script de Python: ${stderr}`);
+//     return;
+//   }
+//   console.log(`Resultado del script de Python:\n${stdout}`);
+// });
